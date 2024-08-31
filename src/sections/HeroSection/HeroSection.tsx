681b075b2fbe37cab42e15e8988dceb3032e22cb
yaalalf <yaalalf98@gmail.com>
2024-08-31 00:29:45 -0400

    fixing errors v-04

--- src/sections/HeroSection/HeroSection.tsx
+++ src/sections/HeroSection/HeroSection.tsx
@@ -1,21 +1,5 @@
-import YLLayoutOne from "@/components/yl-layouts/layout-one/layout-one";
-import LayoutTwo from "@/components/yl-layouts/layout-two/layout-two";
 import YLBackgroundSection from "@/components/yl-layouts/yl-background-section/yl-background-section";
 
 export default function HeroSection() {
-  return (
-    // <YLLayoutOne
-    //   backgroundImage={{
-    //     url: "/background-hero.jpg",
-    //     position: ["center", "bottom"],
-    //   }}
-    //   decoratorImage={{
-    //     url: "/decorator-hero.png",
-    //   }}
-    // ></YLLayoutOne>
-
-    // <LayoutTwo />
-
-    <YLBackgroundSection />
-  );
+  return <YLBackgroundSection />;
 }
