64f1ad674308eaf86aaee3f38bef4d8d2429fd3e
yaalalf <yaalalf98@gmail.com>
2024-09-09 09:43:40 -0400

    fixing error v5

--- src/sections/WyChooseUsSection/WyChooseSection.tsx
+++ src/sections/WyChooseUsSection/WyChooseSection.tsx
@@ -14,78 +14,79 @@ import { Fragment } from "react";
 
 export default function WyChooseSection() {
   return (
-    <YLBackgroundContainer
-      containerStyle={{ tag: "section", paddingBlock: "80px" }}
-      backgroundColorStyle={{ opacity: "0.5" }}
-    >
-      <YLFlexContainer
-        inlineSize="100%"
-        flexDirection="column"
-        justifyContent="flex-start"
-        gap="4px"
-      >
-        <YLTextHeader
-          tag={ETag.h2}
-          fontSize="28px"
-          textTransform={EYLTextHeaderTransform.uppercase}
-          fontFamily="Nunito"
-          fontWeight="900"
-          color="white"
-          textShadow="0px 3px 6px #00000050"
-          textAlign={EYLTextHeaderTextAlign.center}
-        ></YLTextHeader>
-
-        <YLTextHeader
-          tag={ETag.h3}
-          fontSize="18px"
-          textTransform={EYLTextHeaderTransform.uppercase}
-          fontFamily="Nunito"
-          fontWeight="400"
-          color="white"
-          textShadow="0px 3px 6px #00000050"
-          textAlign={EYLTextHeaderTextAlign.center}
-        ></YLTextHeader>
-
-        <YLFlexContainer inlineSize="100%" gap="32px" marginBlockStart="40px">
-          {[0, 0, 0].map((item, index) => (
-            <Fragment key={index}>
-              <YLFlexContainer
-                flexDirection="column"
-                inlineSize="320px"
-                gap="20px"
-              >
-                <YLFlexContainer
-                  inlineSize="100px"
-                  blockSize="100px"
-                  borderRadius="100%"
-                  backgroundColor="white"
-                  boxShadow="0px 3px 6px #00000020"
-                  overflow="hidden"
-                >
-                  <YLMaskedImage
-                    backgroundColor="#999"
-                    position="relative"
-                    inlineSize="50%"
-                    blockSize="50%"
-                  ></YLMaskedImage>
-                </YLFlexContainer>
-
-                <YLTextHeader
-                  tag={ETag.h3}
-                  fontSize="22px"
-                  textTransform={EYLTextHeaderTransform.uppercase}
-                  fontFamily="Nunito"
-                  fontWeight="800"
-                  color="white"
-                  textShadow="0px 3px 6px #00000050"
-                  textAlign={EYLTextHeaderTextAlign.center}
-                ></YLTextHeader>
-                <YLText tag={ETag.p} fontSize="16px" color="#eee"></YLText>
-              </YLFlexContainer>
-            </Fragment>
-          ))}
-        </YLFlexContainer>
-      </YLFlexContainer>
-    </YLBackgroundContainer>
+    <>
+      {
+        // <YLBackgroundContainer
+        //   containerStyle={{ tag: "section", paddingBlock: "80px" }}
+        //   backgroundColorStyle={{ opacity: "0.5" }}
+        // >
+        //   <YLFlexContainer
+        //     inlineSize="100%"
+        //     flexDirection="column"
+        //     justifyContent="flex-start"
+        //     gap="4px"
+        //   >
+        //     <YLTextHeader
+        //       tag={ETag.h2}
+        //       fontSize="28px"
+        //       textTransform={EYLTextHeaderTransform.uppercase}
+        //       fontFamily="Nunito"
+        //       fontWeight="900"
+        //       color="white"
+        //       textShadow="0px 3px 6px #00000050"
+        //       textAlign={EYLTextHeaderTextAlign.center}
+        //     ></YLTextHeader>
+        //     <YLTextHeader
+        //       tag={ETag.h3}
+        //       fontSize="18px"
+        //       textTransform={EYLTextHeaderTransform.uppercase}
+        //       fontFamily="Nunito"
+        //       fontWeight="400"
+        //       color="white"
+        //       textShadow="0px 3px 6px #00000050"
+        //       textAlign={EYLTextHeaderTextAlign.center}
+        //     ></YLTextHeader>
+        //     <YLFlexContainer inlineSize="100%" gap="32px" marginBlockStart="40px">
+        //       {[0, 0, 0].map((item, index) => (
+        //         <Fragment key={index}>
+        //           <YLFlexContainer
+        //             flexDirection="column"
+        //             inlineSize="320px"
+        //             gap="20px"
+        //           >
+        //             <YLFlexContainer
+        //               inlineSize="100px"
+        //               blockSize="100px"
+        //               borderRadius="100%"
+        //               backgroundColor="white"
+        //               boxShadow="0px 3px 6px #00000020"
+        //               overflow="hidden"
+        //             >
+        //               <YLMaskedImage
+        //                 backgroundColor="#999"
+        //                 position="relative"
+        //                 inlineSize="50%"
+        //                 blockSize="50%"
+        //               ></YLMaskedImage>
+        //             </YLFlexContainer>
+        //             <YLTextHeader
+        //               tag={ETag.h3}
+        //               fontSize="22px"
+        //               textTransform={EYLTextHeaderTransform.uppercase}
+        //               fontFamily="Nunito"
+        //               fontWeight="800"
+        //               color="white"
+        //               textShadow="0px 3px 6px #00000050"
+        //               textAlign={EYLTextHeaderTextAlign.center}
+        //             ></YLTextHeader>
+        //             <YLText tag={ETag.p} fontSize="16px" color="#eee"></YLText>
+        //           </YLFlexContainer>
+        //         </Fragment>
+        //       ))}
+        //     </YLFlexContainer>
+        //   </YLFlexContainer>
+        // </YLBackgroundContainer>
+      }
+    </>
   );
 }
